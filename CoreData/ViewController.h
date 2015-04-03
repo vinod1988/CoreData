ee6170fe46a21f9ed5d244bf373939307b31bc3c
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-04-03 15:39:50 +0530

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Vinod Vishwakarma on 03/04/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Vinod Vishwakarma on 03/04/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
